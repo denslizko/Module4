8cb95dd52eb460e8ac4f95219fec4dd99a159bd8
DenSlizko <denis.slizko@gmail.com>
2016-03-13 15:37:10 +0200

    Update code

--- src/Distance.java
+++ src/Distance.java
@@ -1,10 +1,6 @@
 public class Distance {
     public static void main(String[] args) {
-        int x1 = 1;
-        int y1 = 3;
-        int x2 = 5;
-        int y2 = 10;
-        int dis = Math.abs((x2-x1)+(y2-y1));
-        System.out.println(dis);
+       DataForDistance dataForDistance = new DataForDistance("Дистанция между точками = ", 1, 3, 5, 10);
+        System.out.println(dataForDistance.getDis() + dataForDistance.getFormula());
     }
 }
--- src/Temperature.java
+++ src/Temperature.java
@@ -1,12 +1,8 @@
 public class Temperature {
     public static void main(String[] args) {
-        String text1 = " градуса по Цельсию при переводе в Фаренгейты будет ";
-        String text2 = " градуса по Фаренгейту при переводе в Цельсии будет ";
-        int tc1 = 24;
-        int tf2 = 85;
-        double tf1 = tc1*1.8+32;
-        double tc2 = (tf2-32)/1.8;
-        System.out.println(tc1 + text1 + tf1);
-        System.out.println(tf2 + text2 + tc2);
+        DataForTemperature dataForTemperature = new DataForTemperature(40, 104);
+
+        System.out.println(dataForTemperature.getTc1() + dataForTemperature.getText1()+dataForTemperature.getTf1());
+        System.out.println(dataForTemperature.getTf2() + dataForTemperature.getText2()+dataForTemperature.getTc2());
     }
 }
